57da2e880d9ee026a4999f3c7a10f163468d5313
VineelaKurapati52 <vineelakurapati52@gmail.com>
2026-06-16 10:11:45 +0530

    Delete DineQuick directory

--- DineQuick/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dinequick-secret-key-2024')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'restaurant.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
